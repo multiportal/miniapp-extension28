8db511879b7ec389ee3da0250df8de6ff0cd0d44
gjimenez <gjimenez@wikitek.com.mx>
2026-06-18 20:37:55 -0600

    Update: Card

--- src/pages/productos.js
+++ src/pages/productos.js
@@ -3,32 +3,34 @@ import { getData } from '../app/services/firebase.js';
 export function productos() {
 
     const products = async () => {
-        const data = await getData('/vcard_vcard');
-        console.log(data);
+        let html = '';
+        const data = await getData('/vcard_vcard'); console.log(data);
         const productList = document.getElementById('product-list');
+        //Cards
         for (const item of data) {
-            const { ID, key, nombre, email } = item;
-            productList.innerHTML += `
+            const { ID, key, nombre, email, visible } = item;
+            if (visible) {
+                html += `
                 <div class="card mb-3" key="${key}">
                     <div class="card-body">
                         <h5 class="card-title mb-0">${nombre}</h5>
                         <p class="card-text">${email}</p>
                     </div>
                 </div>
-            `;
+                `;
+            }
         }
+        productList.innerHTML = html;
     };
 
-    products();
+    setTimeout(() => { products(); }, 1000);
 
     return `
   <div class="container">
     <div class="row">
       <h1>PRODUCTOS</h1>
       <p>Esta es la página de productos.</p>
-      <div class="col-md-12" id="product-list">
-        Cargando productos...
-      </div>
+      <div class="col-md-12" id="product-list">Cargando...</div>
     </div>
   </div>
   `;
